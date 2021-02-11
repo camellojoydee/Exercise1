060bba2a77f3bad3b2b29a49816d333ec0472b9b
camellojoydee@gmail.com <camellojoydee@gmail.com>
2021-02-11 15:25:46 +0800

    First commit

---
+++ Pages/News.cshtml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Exercise1.Pages
+{
+    public class NewsModel : PageModel
+    {
+        private readonly ILogger<NewsModel> _logger;
+
+        public NewsModel(ILogger<NewsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
